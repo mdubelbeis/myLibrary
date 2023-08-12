4123e0793f02030c52900313fdb7ae29d34dbbf1
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-08-12 10:32:27 -0500

    installed tailwind and scss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    import '$lib/app.css';    
+    export let data: LayoutData;
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,7 @@
 
 <h1>myLibrary</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+
+</style>
