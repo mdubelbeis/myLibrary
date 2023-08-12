6650e0a3d512af5a2594f29b0222736bfaaa1cec
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-08-12 11:05:38 -0500

    extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'rich-black': '#0d1b2aff',
+				'oxford-blue': '#1b263bff',
+				'yinmn-blue': '#415a77ff',
+				'silver-lake-blue': '#778da9ff',
+				platinum: '#e0e1ddff'
+			}
+		}
 	},
 	plugins: []
 };
