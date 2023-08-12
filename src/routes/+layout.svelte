cf55fcccb66d3b8f0efe027b23bcb557da2a9ceb
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-08-12 11:05:26 -0500

    added header v1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,26 @@
     export let data: LayoutData;
 </script>
 
+<header class="bg-oxford-blue flex justify-between text-silver-lake-blue p-3 shadow-lg">
+  <div>
+    myLibrary
+  </div>
+  <nav class="mr-2">
+    <ul class="flex gap-2">
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/library">Library</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
 <slot />
+
+<style lang="scss">
+  li a:hover {
+    color: var(--platinum)
+  }
+</style>
